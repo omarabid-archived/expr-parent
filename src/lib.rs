34862c26e5cab75baceecd1c0db2f07cb601e986
Abid Omar <contact@omarabid.com>
2019-11-05 01:21:56 +0100

    Init

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+extern crate expr_child;
+
+fn really_nothing() {
+    // nothing here too
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
